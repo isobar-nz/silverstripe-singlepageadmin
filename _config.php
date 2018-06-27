97b8aa294f7957fde98ae943d977f10ce4264e9f
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-06-27 15:01:41 +1200

    Make SinglePageAdmin class abstract

--- _config.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-\SilverStripe\Admin\CMSMenu::remove_menu_item('LittleGiant-SinglePageAdmin-SinglePageAdmin');
--- src/LittleGiant/SinglePageAdmin/SinglePageAdmin.php
+++ src/LittleGiant/SinglePageAdmin/SinglePageAdmin.php
@@ -30,7 +30,7 @@ use SilverStripe\Control\HTTPResponse;
  * @package SinglePageAdmin
  * @author Stevie Mayhew
  */
-class SinglePageAdmin extends LeftAndMain implements PermissionProvider
+abstract class SinglePageAdmin extends LeftAndMain implements PermissionProvider
 {
     /**
      * As of 4.0 all subclasses of LeftAndMain have to have a
